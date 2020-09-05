9a5989674af27bcb1c4d150b2c47ff8dc3ee3d1a
Joey L <x@ezget.info>
2020-09-04 23:05:37 -0400

    theme update

--- docs/menu.js
+++ docs/menu.js
@@ -1,5 +1,5 @@
 
-function GenerateTopNavigationBar(dotPath)
+function GenerateTopNavigationBar(dotPath, page)
 {
     var WebSiteRoot = 'http://evin.ezget.info/';
     WebSiteRoot = 'file:///C:/src/ezLiang/web/docs/';
@@ -7,18 +7,24 @@ function GenerateTopNavigationBar(dotPath)
     var bookGroupSubMenu = ''    
     for (var index = 0; index < books.length; index++)
     {
-        bookGroupSubMenu += '            <a href="' + dotPath + 'books/books.html?group=' + index + '">' + books[index].group + '</a>\n';
+        bookGroupSubMenu += '      <a href="' + dotPath + 'books/books.html?group=' + index + '">' + books[index].group + '</a>\n';
     }
 
+    var HomeClass = (page == "Home") ? ' class="active" ' : '';
+    var bookClass = (page == "Reading-List") ? ' class="active" ' : '';
     document.write(`
       <div class="topNavBar">
-        <a href="` + dotPath + `">Home</a>
+        <a ` + HomeClass + ` href="` + dotPath + `">Home</a>
         <div class="dropDown">
-          <a href="` + dotPath + `books/index.html" class="dropButton">Reading-List</a>
-          <div class="dropDown-content">\n`
-          + bookGroupSubMenu + `
+          <a ` + bookClass + ` href="` + dotPath + `books/index.html" class="dropButton">Reading-List</a>
+          <div class="dropDown-content">`
+            + bookGroupSubMenu +`
           </div>
-        </div> 
+        </div>
+
+        <div style="float: right; margin:5pt; font-size:16pt; color: yellow;">
+          Evin Liang
+        </div>
       </div>`);  
 }
 
